703dc9ee4baf486a89573d5c3ea6610b6f1bd1d9
btenreiro <tenreiro.benjamin@gmail.com>
2022-07-25 11:16:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### tommysonsylverstone/karuta-fudanagashi-react 
---
+++ src/components/Card/Cards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Card from './Card';
+const Cards = () => {
+  return (
+    <div>Cards</div>
+  )
+}
+
+export default Cards
